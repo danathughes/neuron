28d26d8d72c31a9460d5960a2dff61928eda2a15
Joshua Goller <jsgoller1@gmail.com>
2016-09-09 06:03:47 -0700

    debbuging, hmmmm

--- main.cpp
+++ main.cpp
@@ -27,13 +27,23 @@ const GLchar* fragment_shader_source = "#version 330 core\n"
     "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
 
-void manage_shader(const GLchar* const shader_source, const GLenum shader_type);
-void manage_shader(const GLchar* const shader_source, const GLenum shader_type){
+int compile_shader(const GLchar* const shader_source, const GLenum shader_type, GLuint shader_id);
+int compile_shader(const GLchar* const shader_source, const GLenum shader_type, GLuint shader_id){
 	// Create and compile shaders
-	GLuint shader_id;
 	shader_id = glCreateShader(shader_type);
 	glShaderSource(shader_id, 1, &shader_source, NULL);
 	glCompileShader(shader_id);
+
+	// Check for compilation failure
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+	if(!success) {
+		glGetShaderInfoLog(shader_id, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+		return -1;
+	}
+	return 0;
 }
 
 int main()
@@ -68,6 +78,57 @@ int main()
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 
+	// Compile and check shaders
+	GLuint vertex_shader = 0;
+	if (compile_shader(vertex_shader_source, GL_VERTEX_SHADER, vertex_shader)!= 0){
+		std::cout << "Failed to compile vertex shaders.";
+		return -1;
+	}
+
+	GLuint fragment_shader = 0;
+	if (compile_shader(fragment_shader_source, GL_FRAGMENT_SHADER, fragment_shader) !=0 ){
+		std::cout << "Failed to compile fragment shaders.";
+		return -1;
+	}
+
+	// Build our shader program and link the compiled shaders
+	GLuint shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertex_shader);
+	glAttachShader(shaderProgram, fragment_shader);
+	glLinkProgram(shaderProgram);
+	// Check linking here
+	GLint success;
+	GLchar infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader); 
+
+	// Initialize vertices and data on the GPU
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	GLuint VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
 	// Initialize game loop
 	while(!glfwWindowShouldClose(window)) {
 		// Check for input / events
@@ -78,26 +139,18 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Initialize vertices and data on the GPU
-		GLfloat vertices[] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f,  0.5f, 0.0f
-		};
-
-		GLuint VBO;
-		glGenBuffers(1, &VBO);
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		// Handle shaders
-		manage_shader(vertex_shader_source, GL_VERTEX_SHADER);
-		//manage_shader(fragment_shader_source, GL_FRAGMENT_SHADER);
+		// Draw!
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		// Draw backbuffer to screen
 		glfwSwapBuffers(window);
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 	return 0;
 }
