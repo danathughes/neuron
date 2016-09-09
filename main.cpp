f754c17ed5bc79956bf6c7e59c6e1eb078e40ee1
Joshua Goller <jsgoller1@gmail.com>
2016-09-09 04:19:54 -0700

    incremental progress

--- main.cpp
+++ main.cpp
@@ -7,13 +7,27 @@
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
     // When a user presses the escape key, we set the WindowShouldClose property to true, 
     // closing the application
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     	glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
+// Shaders, bleh
+const GLchar* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 position;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+    "}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main()\n"
+    "{\n"
+    "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 
 int main()
 {
@@ -27,8 +41,7 @@ int main()
 
 	// Create GLFW window object
 	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
-	if (window == nullptr)
-	{
+	if (window == nullptr) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
@@ -38,8 +51,7 @@ int main()
 
 	// Initialize GLEW
 	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
-	{
+	if (glewInit() != GLEW_OK) {
 		std::cout << "Failed to initialize GLEW" << std::endl;
 		return -1;
 	}
@@ -47,14 +59,29 @@ int main()
 	// Intialize viewport
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
-
 	glViewport(0, 0, width, height);
 
 	// Initialize game loop
-	while(!glfwWindowShouldClose(window))
-	{
+	while(!glfwWindowShouldClose(window)) {
 		// Check for input / events
 		glfwPollEvents();
+
+		// Begin rendering stuff here
+		// Set background color to cyan-ish
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		GLfloat vertices[] = {
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.0f,  0.5f, 0.0f
+		};
+
+		GLuint VBO;
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
 		// Draw backbuffer to screen
 		glfwSwapBuffers(window);
 	}
