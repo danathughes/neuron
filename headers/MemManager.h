5ece00cef83918fae8a21bc9db7e8d59a7fcc150
Joshua Goller <jsgoller1@gmail.com>
2016-09-27 02:01:15 -0700

    memory manager skeleton

---
+++ headers/MemManager.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class MemManager 
+{
+public:
+	MemManager();
+	~MemManager();
+	void MemManager::StartUp();
+	void MemManager::ShutDown();
+};
+
---
+++ src/MemManager.cpp
@@ -0,0 +1,21 @@
+#include <MemManager.h>
+
+MemManager::MemManager()
+{
+
+}
+
+MemManager::~MemManager()
+{
+
+}
+
+void MemManager::StartUp()
+{
+
+}
+
+void MemManager::ShutDown()
+{
+
+}
