0b51f711b6906547da617978a0e09fa4b8378e35
Joshua Goller <jsgoller1@gmail.com>
2016-10-28 19:05:26 -0700

    conventionalizes GibVect3

--- headers/GibVect3.h
+++ headers/GibVect3.h
@@ -5,19 +5,19 @@
 class GibVect3
 {
 public:
-	GibVect3(); // TODO: This will literally never get moved; remove it?
-	GibVect3(const GibVect3* vect);
-	GibVect3(const std::vector<float>* vect);
-	GibVect3(float x, float y, float z);
+	GibVect3();
+	GibVect3(const GibVect3* const vect);
+	GibVect3(const std::vector<float>* const vect);
+	GibVect3(const float x, const float y, const float z);
 	~GibVect3();
 	void Add(const GibVect3* const distance);
 	bool isEqual(const GibVect3* const vect) const;
 	float x() const;
-	void x(float newX);
+	void x(const float newX);
 	float y() const;
-	void y(float newY);
+	void y(const float newY);
 	float z()const ;
-	void z(float newZ);
+	void z(const float newZ);
 
 private:
 	std::vector<float> elements;
--- src/GibVect3.cpp
+++ src/GibVect3.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
-#include <GibVect3.h>
 #include <vector>
+#include <GibVect3.h>
 
+// ctors/dtors
 GibVect3::GibVect3()
 {
 	this->elements = std::vector<float>();
+}
+
+GibVect3::GibVect3(const GibVect3* const vect)
+{
+	this->elements = std::vector<float>(3);
+	this->elements[0] = vect->elements[0];
+	this->elements[1] = vect->elements[1];
+	this->elements[2] = vect->elements[2];
+
+}
+
+GibVect3::GibVect3(const std::vector<float>* const vect)
+{
+	this->elements = std::vector<float>(3);
+	this->elements[0] = vect->at(0);
+	this->elements[1] = vect->at(1);
+	this->elements[2] = vect->at(2);
 }
 
 GibVect3::GibVect3(const float x, const float y, const float z)
@@ -15,51 +33,13 @@ GibVect3::GibVect3(const float x, const float y, const float z)
 	this->elements[2] = z;
 }
 
-GibVect3::GibVect3(const GibVect3* vect)
-{
-	this->elements = std::vector<float>(3);
-	this->elements[0] = vect->elements[0];
-	this->elements[1] = vect->elements[1];
-	this->elements[2] = vect->elements[2];
-
-}
-
-GibVect3::GibVect3(const std::vector<float>* vect)
-{
-	this->elements = std::vector<float>(3);
-	this->elements[0] = vect->at(0);
-	this->elements[1] = vect->at(1);
-	this->elements[2] = vect->at(2);
-}
 
 GibVect3::~GibVect3()
 {
 
 }
 
-float GibVect3::x() const {
-	return this->elements[0];
-}
-
-void GibVect3::x(float newX) {
-	this->elements[0] = newX;
-}
-
-float GibVect3::y() const {
-	return this->elements[1];
-}
-
-void GibVect3::y(float newY) {
-	this->elements[1] = newY;
-}
-
-float GibVect3::z() const {
-	return this->elements[2];
-}
-
-void GibVect3::z(float newZ) {
-	this->elements[2] = newZ;
-}
+// public
 
 void GibVect3::Add(const GibVect3* const distance) {
 	this->elements[0] += distance->x();
@@ -72,4 +52,28 @@ bool GibVect3::isEqual(const GibVect3* const otherVect) const {
 	std::cout << "this->y(): " << this->y() << " || otherVect->y(): " << otherVect->y() << "\n";
 	std::cout << "this->z(): " << this->z() << " || otherVect->z(): " << otherVect->z() << "\n";
 	return (this->x() == otherVect->x()) && (this->y() == otherVect->y()) && (this->z() == otherVect->z());
+}
+
+float GibVect3::x() const {
+	return this->elements[0];
+}
+
+void GibVect3::x(const float newX) {
+	this->elements[0] = newX;
+}
+
+float GibVect3::y() const {
+	return this->elements[1];
+}
+
+void GibVect3::y(const float newY) {
+	this->elements[1] = newY;
+}
+
+float GibVect3::z() const {
+	return this->elements[2];
+}
+
+void GibVect3::z(const float newZ) {
+	this->elements[2] = newZ;
 }
